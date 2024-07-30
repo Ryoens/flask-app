22eada425f08ba0580cc202270b16ea4ab45acbb
Ryoens <mj66004@e.hiroshima-cu.ac.jp>
2024-07-30 15:16:47 +0000

    add file

---
+++ random.py
@@ -0,0 +1,17 @@
+from flask import Flask
+import random
+
+app = Flask(__name__)
+
+# ランダムに表示する文字列
+sentences = [
+    "<p>Hello World!<p>",
+    "<p>こんにちは 世界!<p>",
+    "<p>Hallo Welt<p>",
+    "<p>Bonjour le monde<p>"
+]
+
+@app.route("/")
+def hello_world():
+    sentence = random.choice(sentences)
+    return sentence
